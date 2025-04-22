6f4ae21e0b2ade4770111d8045db22896dce9646
matheusfernandesrodrigues <matheusfernandesrodrigues@outlook.com>
2025-04-22 12:13:05 -0300

    first commit

---
+++ src/test/java/com/twozerotwo/fastfood/FastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twozerotwo.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
