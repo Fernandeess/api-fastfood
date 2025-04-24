b3f9c66d5e158b720013085dd67b474c7c7e1516
Fernandes <matheusfernandesrodrigues@outlook.com>
2025-04-24 15:46:32 -0300

    feat: configure DI for CreateCustomerUseCase

--- src/main/java/com/twozerotwo/fastfood/application/configuration/UseCaseBeanConfiguration.java
+++ src/main/java/com/twozerotwo/fastfood/application/configuration/UseCaseBeanConfiguration.java
@@ -1,7 +1,6 @@
 package com.twozerotwo.fastfood.application.configuration;
 
 import com.twozerotwo.fastfood.adapters.driven.persistence.repository.CustomerRepositoryAdapter;
-import com.twozerotwo.fastfood.core.ports.CustomerRepositoryPort;
 import com.twozerotwo.fastfood.core.services.CreateCustomerServiceImpl;
 import com.twozerotwo.fastfood.core.usecases.CreateCustomerUseCase;
 import org.springframework.context.annotation.Bean;
